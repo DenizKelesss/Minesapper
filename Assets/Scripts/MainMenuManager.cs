26df2fea5ce2176874f54f68b1e078d80c85ff88
DenizKelesss <denizkelessss@gmail.com>
2025-04-16 14:54:19 +0300

    save system update
    -start game deletes old save
    -continue button is only available after game is saved

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,11 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public Button continueButton;
+
+    private void Start()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (System.IO.File.Exists(path))
+        {
+            continueButton.interactable = true;
+        }
+        else
+        {
+            continueButton.interactable = false;
+        }
+    }
+
+
+
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (System.IO.File.Exists(path))
+        {
+            System.IO.File.Delete(path);
+        }
+
+        SceneManager.LoadScene("Level1");
     }
 
     public void QuitGame()
@@ -23,6 +49,11 @@ public class MainMenuManager : MonoBehaviour
             string json = System.IO.File.ReadAllText(path);
             SaveData data = JsonUtility.FromJson<SaveData>(json);
             lastLevel = data.currentLevel;
+        }
+        else
+        {
+            Debug.Log("No saved game found.");
+            // You could show a UI popup here.
         }
 
         SceneManager.LoadScene("Level" + lastLevel);
